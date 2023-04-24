45ae03d84156e266571744b360dfed59ae1601e1
Duc Tuyen <54266297+tuyenduc@users.noreply.github.com>
2023-04-24 17:43:48 +0700

    [2023-04-24]_[update table phat am]

--- assets/js/phat-am.js
+++ assets/js/phat-am.js
@@ -1,5 +1,20 @@
+import mappingss from './../../assets/jsons/maping-phat-am.json' assert { type: 'json' };
 $(document).ready(function () {
     $("body").on("click", ".table-item-call-listen", function () {
         console.log("Show modal");
     });
+    LoadTable();
+    function LoadTable() {
+        let htmlContent = "";
+        let ArrMappings = mappingss.Mappings;
+        ArrMappings.map(item => {
+            htmlContent += `<ul class="table-row"><li class="table-item bg-info text-white">${item.VanMau}</li>`;
+            let arrmaps = item.ThanhVanMau;
+            arrmaps.map(tm => {
+                htmlContent += `<li class="table-item ${(tm != "" ? "table-item-call-listen" : "")}">${tm}</li>`;
+            });
+            htmlContent += "</ul>";
+        });
+        $(".table-phat-am").append(htmlContent);
+    }
 });
